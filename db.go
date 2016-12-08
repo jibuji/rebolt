0793298682792f9646681ba30359308e1b618aee
mabuji <sunnypengfei@gmail.com>
2016-12-08 13:20:06 +0800

    complements docs

--- db.go
+++ db.go
@@ -6,7 +6,7 @@
 // and easy to use operations, so you can change your db between bolt and redis
 // without pains.
 //
-// Remmeber the key and value types in both bolt and redis:
+// Remember the key and value types in both bolt and redis, here they are:
 // `string, bool`, any integer types, any float types, and arrays of this types.
 // The underhood representation is `[]byte`, it's your responsibility to interpret it.
 package rebolt
@@ -38,30 +38,60 @@ type IDB interface {
 type ITX interface {
 	//Set stores the key-value pair in db.
 	Set(key interface{}, value interface{})
+
 	//Get retrieves the value by the given key.
 	Get(key interface{}) []byte
+
 	//Del rm the key-value pair by the given key
 	Del(key interface{})
+
 	//Keys get all keys that match the given `glob` pattern
 	Keys(pattern interface{}) [][]byte
+
 	//SIsMember return if the field is in the set indicated by the given key
 	SIsMember(key, field interface{}) bool
+
+	//SMembers retrieve all the members in the set indicated by the given key
 	SMembers(key interface{}) [][]byte
+
+	//SAdd add the member into the set indicated by the given key
 	SAdd(key, member interface{})
+
+	//SRem remove the member from the set indicated by the given key
 	SRem(key, member interface{})
+
+	//HGet get the field value from the hash indicated by the given key
 	HGet(key, field interface{}) []byte
+
+	//HSet set a pair(field-value) into the hash indicated by the given key
 	HSet(key, field, value interface{})
-	//params:key, field1, val1,  ....
+
+	//HMSet multi-set a hash, params:key, field1, val1, field2, val2, ....
 	HMSet(kfrs ...interface{})
-	//params:key, field1, field2,  ....
+
+	//HMGet multi-get a hash, params:key, field1, field2, field2, val2, ....
 	HMGet(kfields ...interface{}) [][]byte
+
+	//HGetAll get all the (field-val) pairs from the hash indicated by the given key
 	HGetAll(key interface{}) [][]byte
+
+	//Watch watch the keys's changes.
+	//It only makes sense on redis, and have no side-effects on bolt.
 	Watch(keys ...interface{})
+
+	//Multi begin queue cmds
+	//It only makes sense on redis, and have no side-effects on bolt.
 	Multi()
+
+	//Exec excecute queued cmds
+	//It only makes sense on redis, and have no side-effects on bolt.
 	Exec() error
 }
 
-//GetDB GetDB for
+//GetDB grab the db interface by `dbType` and `dbIndex`.
+// The current supported `dbType` are redis and bolt.
+// `dbIndex` is an integer indicates different storage space,
+// just like the `index` in `select index` of redis.
 func GetDB(dbType string, dbIndex int) (db IDB, err error) {
 	switch dbType {
 	case "bolt":
